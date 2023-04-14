86c88849890a8a0ed98154a51fd49285fcff71a5
Sivarama Krishna <koutharapusivaram@gamail.com>
2023-04-14 12:56:43 +0000

    routing practice

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="navbar">
+    <div className="logo-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+        className="logo"
+      />
+      <p>Wave</p>
+    </div>
+    <ul className="list-container">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+export default Header
